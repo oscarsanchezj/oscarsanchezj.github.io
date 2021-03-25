8c124bebedf40d59d57092d9796aacab7b2e7802
Oscar Sanchez Juanes <oscar.sanchez@usal.es>
2021-03-25 17:35:14 +0100

    update

--- validacion.js
+++ validacion.js
@@ -1,13 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("formulario").addEventListener('submit', validarFormulario); 
-});
-
-function validarFormulario(evento) {
-  evento.preventDefault();
-  var nombre = document.getElementById('nombre').value;
-  if(nombre.length == 0) {
-    alert('No has escrito nada en el nombre');
-    return;
-  }
-  this.submit();
-}
+alert("Un mensaje de prueba")
